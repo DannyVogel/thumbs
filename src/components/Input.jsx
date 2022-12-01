18d2a6b9491453feea41db8ebd6103cea0532a97
Danny <danny.hj.vogel@gmail.com>
2022-12-01 19:09:25 +0100

    unified state into single object, cleaned up code

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,39 +2,47 @@ import React,{useState} from "react";
 
 
 function Input() {
-    const [clickedL, setclickedL] = useState(false)
-    const [clickedR, setclickedR] = useState(false)
-    const [inputMinL, setInputMinL] = useState(0)
-    const [inputMinR, setInputMinR] = useState(0)
-    
-    const inputMin = inputMinL + inputMinR
+    const [clicked, setClicked] = useState({
+        clickedL: false,
+        clickedR: false,
+        numThumb: 0
+    })
 
-    function handleClickL() {
-        console.log(clickedL)
-        setclickedL((prevState) => !prevState)
-        setInputMinL(inputMinL === 0? 1 : 0) 
+    function handleClick(event) {
+        const {name, value} = event.target 
+
+        setClicked(prevState => ({
+            ...prevState,
+            [name]: !prevState[name],
+            numThumb: (!prevState[name] ? prevState.numThumb+1 : prevState.numThumb-1)
+
+        }))
     }
 
-    function handleClickR() {
-        setclickedR((prevState) => !prevState)
-        setInputMinR(inputMinR === 0? 1 : 0) 
-    }
+const styles1 = {
+    backgroundColor: clicked.clickedL === true ? "blue" : "gray"
+}
+const styles2 = {
+    backgroundColor: clicked.clickedR === true ? "blue" : "gray"
+}
+
+console.log(clicked)
 
     return (
-        <div className="container text-center">
+        <div>
                 <div>
-                    <button onClick={handleClickL} className={clickedL === true ? "btn btn-primary" : "btn btn-secondary"}>Left Thumb</button>{' '}
-                    <button onClick={handleClickR} className={clickedR === true ? "btn btn-primary" : "btn btn-secondary"}>Right Thumb</button>{' '}
+                    <button onClick={handleClick} name="clickedL" value={clicked.clickedL} style={styles1}>Left Thumb</button>{' '}
+                    <button onClick={handleClick} name="clickedR" value={clicked.clickedR} style={styles2}>Right Thumb</button>{' '}
                 </div>
                 <br />
-                <div className="d-grid gap-2">
+                <div>
                     <label htmlFor="guess"><strong>How many thumbs do you think will be raised in total?</strong></label>
 {/* use HTML Select element or create buttons for pressing number of thumbs up (2nd better for when 1 point won) */}
-                    <input className="col-2 align-center mx-auto" type="number" id="guess" maxlength="1" placeholder={inputMin === 0 ? "0" : inputMin} min={inputMin} max="4" required autoFocus/>
+                    <input type="number" id="guess" maxLength="1" placeholder={clicked.numThumb} min={clicked.numThumb} max="4" required autoFocus/>
                 </div>
                 <br />
-                <div className="d-grid gap-2">
-                    <button type="submit" className="btn btn-success">Play</button>
+                <div>
+                    <button type="submit">Play</button>
                 </div>
         </div>
     )
