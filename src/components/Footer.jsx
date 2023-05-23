6aa0343b77e050c92a575f885a76242e8ca00b87
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-23 10:53:18 +0200

    update footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-      <p>© Danny Vogel 2022</p>
+      <p>© Danny Vogel 2023</p>
       <p>
       Fist icons created by:&nbsp;
         <a target="_blank" href="https://www.flaticon.com/free-icons/fist-bump" title="fist bump icons">Khoirul Huda,</a>
         &nbsp;<a target="_blank" href="https://www.flaticon.com/free-icons/fist" title="fist icons">Jesus Chavarria,</a>
         &nbsp;and <a target="_blank" href="https://www.flaticon.com/free-icons/fist" title="fist icons">Freepik</a> - Flaticon
       </p>        
+      
     </div>
   )
 }
