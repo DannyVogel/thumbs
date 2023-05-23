36fa79b727a14d01ebea325d2d5f5310c7e48fe1
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-23 14:52:19 +0200

    update player class methods

--- src/components/playerClass.js
+++ src/components/playerClass.js
@@ -4,6 +4,7 @@ class Player {
         this.score = 0;
         this.leftThumb = false;
         this.rightThumb = false;
+        this.thumbsRaised = 0;
         this.guess = 0;
     }
     getName() {
@@ -11,15 +12,26 @@ class Player {
     }
     setLeftThumb() {
         this.leftThumb = !this.leftThumb;
+        this.leftThumb ? this.thumbsRaised++ : this.thumbsRaised--;
     }
     setRightThumb() {
         this.rightThumb = !this.rightThumb;
+        this.rightThumb ? this.thumbsRaised++ : this.thumbsRaised--;
     }
     getThumbs() {
         return {leftThumb: this.leftThumb, rightThumb: this.rightThumb};
     }
+    setThumbsRandom() {
+        this.leftThumb = Math.random() >= 0.5;
+        this.rightThumb = Math.random() >= 0.5;
+        this.leftThumb && this.thumbsRaised++
+        this.rightThumb && this.thumbsRaised++
+    }
     setGuess(num) {
         this.guess = num;
+    }
+    getGuess() {
+        return this.guess;
     }
     getScore() {
         return this.score;
@@ -30,12 +42,14 @@ class Player {
     resetRound(){
         this.leftThumb = false;
         this.rightThumb = false;
+        this.thumbsRaised = 0;
         this.guess = 0;
     }
     resetGame() {
         this.score = 0;
         this.leftThumb = false;
         this.rightThumb = false;
+        this.thumbsRaised = 0;
         this.guess = 0;
     }
 }
