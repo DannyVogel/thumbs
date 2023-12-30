c81d01b7e301788fd9b56d8a40daed9dabc6f055
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-12-30 18:43:03 +0100

    wip

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,13 @@
+// import { useState, useEffect } from "react";
+
+// interface CountdownProps {
+//   guess: number;
+// }
+
+// const Countdown = ({ guess }: CountdownProps) => {
+//   console.log(guess);
+
+//   return <div>Countdown</div>;
+// };
+
+// export default Countdown;
