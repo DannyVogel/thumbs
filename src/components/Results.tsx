474a3f43e1d8fc830cf58a6c5263b665b9d8b904
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-12-30 18:42:45 +0100

    style: update result summary design

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,26 +1,23 @@
 interface ResultsProps {
-  round: number;
   turn: string;
   guess: number;
   thumbTotal: number;
   resultMessage: string;
 }
 
-const Results = ({
-  round,
-  turn,
-  guess,
-  thumbTotal,
-  resultMessage,
-}: ResultsProps) => {
+const Results = ({ turn, guess, thumbTotal, resultMessage }: ResultsProps) => {
   return (
     <>
-      <p className="text-center">Round {round} Results:</p>
-      <p className="text-center">
-        {turn} guess: {guess}
-      </p>
-      <p className="text-center">Total thumbs raised: {thumbTotal}</p>
-      <p className="text-center">{resultMessage}</p>
+      <p className="text-center">Results:</p>
+      <p className="text-center text-2xl font-semibold pb-1">{resultMessage}</p>
+      <div className="flex">
+        <p className="text-center border-r border-t py-1 w-40">
+          {turn} guess: {guess}
+        </p>
+        <p className="text-center border-l border-t py-1 w-40">
+          Thumbs up: {thumbTotal}
+        </p>
+      </div>
     </>
   );
 };
